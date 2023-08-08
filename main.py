9c55200881b8fd40425b453885eafb36af27ceae
BranRH <121693468+BranRH@users.noreply.github.com>
2023-08-08 14:26:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ q = np.zeros([state_size, action_size])
 
 # training variables
 num_episodes = 1000
-max_steps = 15  # per episode
+max_steps = 100 # per episode
 
 for episode in range(num_episodes):
     # reset the environment
